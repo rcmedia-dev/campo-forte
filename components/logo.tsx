566ccc355c6c20d2322776df67dce1f73fda2e28
rcmedia-dev <rcmidia.dev@gmail.com>
2025-11-03 17:01:33 +0100

    campo forte image correction

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export function Logo() {
   return (
     <Link href="/">
-        <Image src="/logo.png" width={200} height={200} alt='logo' className='object-cover h-30 w-fit'/>
+        <Image src="/logo.png" width={200} height={200} alt='logo' className='object-contain h-30 w-fit'/>
     </Link>
   )
 }
